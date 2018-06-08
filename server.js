b8274de74c180eec90b585f0c9dbcfebb2c006f9
PritamDN <pritamdebnath72@gmail.com>
2018-06-08 18:21:54 +0530

    Setup for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.set('View engine', 'hbs');
@@ -16,6 +17,6 @@ app.get('/about', (req, res) => {
    res.render('about.hbs');
 	});
 
-app.listen(3000, () => {
-	console.log('Running at 3000');
+app.listen(port, () => {
+	console.log(`Server is up on ${port}`);
 });
